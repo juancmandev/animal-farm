bcc167834e817901eeccc127d84a86feaeb7eac1
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-03-11 14:15:24 -0600

    add ui and search custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,55 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { search, animals } = useAnimalSearch();
   return (
-    <div className="App">
-    </div>
-  )
+    <main>
+      <h1>
+        Animal Farm
+      </h1>
+      <input type='text' placeholder='Search' onChange={(e) => search(e.target.value)} />
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+        {animals.length === 0 && 'No animals found'}
+      </ul>
+    </main>
+  );
 }
 
-export default App
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <strong>
+        {type}:&nbsp;
+      </strong>
+      {name}
+      ({age} years old)
+    </li>
+  );
+}
+
+function useAnimalSearch() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery);
+  }, []);
+
+  const search = async (q) => {
+    const response = await fetch(
+      `http://localhost:8080?${new URLSearchParams({q})}`
+    );
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem('lastQuery', q);
+  };
+
+  return { search, animals };
+}
+
+export default App;
